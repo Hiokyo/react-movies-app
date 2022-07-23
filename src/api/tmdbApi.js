4322f8482a2e475818345feedec3aeda1bbf8f36
Hiokyo <hieutran235@gmail.com>
2022-07-23 13:05:36 +0700

    Complete all components

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -5,6 +5,12 @@ export const category = {
     tv: 'tv'
 }
 
+export const movieType = {
+    upcoming: 'upcoming',
+    popular: 'popular',
+    top_rated: 'top_rated'
+}
+
 export const tvType = {
     popular: 'popular',
     top_rated: 'top_rated',
@@ -12,15 +18,15 @@ export const tvType = {
 }
 
 const tmdbApi = {
-    getMoviesList: (type, parmas) => {
+    getMoviesList: (type, params) => {
         const url = 'movie/' + movieType[type];
-        return axiosClient.get(url, parmas);
+        return axiosClient.get(url, params);
     },
-    getTvList: (type, parmas) => {
+    getTvList: (type, params) => {
         const url = 'tv/' + tvType[type];
-        return axiosClient.get(url, parmas);
+        return axiosClient.get(url, params);
     },
-    getVideo: (cate, id) => {
+    getVideos: (cate, id) => {
         const url = category[cate] + '/' + id + '/videos';
         return axiosClient.get(url, {params: {}});
     },
@@ -32,17 +38,14 @@ const tmdbApi = {
         const url = category[cate] + '/' + id;
         return axiosClient.get(url, params);
     },
-
     credits: (cate, id) => {
         const url = category[cate] + '/' + id + '/credits';
         return axiosClient.get(url, {params: {}});
     },
-
     similar: (cate, id) => {
         const url = category[cate] + '/' + id + '/similar';
         return axiosClient.get(url, {params: {}});
     },
 }
 
-    export default tmdbApi;
-
+export default tmdbApi;
